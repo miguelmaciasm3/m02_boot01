397346dd26adde4289d2367df3b928a70680fe26
miguelmaciasm3 <michikelomtres@gmail.com>
2019-05-09 18:38:54 +0200

    funciones primer nivel como parametros

---
+++ funciones1nivel.py
@@ -0,0 +1,14 @@
+def normal(x):
+    return x
+
+def cuadrado(y):
+    return y**2
+
+
+def sumatodo(limite, f):
+    resultado = 0
+    for i in range(0, limite+1):
+        resultado += f(i)
+    return resultado
+print(sumatodo(100, normal))
+print(sumatodo(3, cuadrado))
