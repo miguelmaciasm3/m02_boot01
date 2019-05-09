1a19fffd5c69b2953d8dfbae3014865c491ec8d4
miguelmaciasm3 <michikelomtres@gmail.com>
2019-05-09 17:57:30 +0200

    funciones clasicas

--- sumatodo.md.py
+++ sumatodo.md.py
@@ -3,4 +3,11 @@ def sumatodo(limite):
     for i in range(0, limite+1):
         resultado += i
     return resultado
-print sumatodo(100)
+
+def sumaloscuadrados(limite):
+    resultado = 0
+    for i in range (0, limite + 1):
+        resultado += i*i
+    return resultado
+print(sumatodo(100))
+print(sumaloscuadrados(3))
