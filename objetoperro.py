1b5b61d70642c7c0f619336b5657bd7f8993e294
miguelmaciasm3 <michikelomtres@gmail.com>
2019-05-10 21:58:04 +0200

    segunda version para poder imprimir

--- objetoperro.py
+++ objetoperro.py
@@ -3,3 +3,9 @@ class Perro():
         self.nombre = nombre
         self.edad = edad
         self.peso = peso
+    def ladrar(self):
+        print("guau, guau!")
+    def __self__(self):
+        return "perro {}, edad: {}, peso: {}".format(self.nombre, self.edad, self.peso)
+def ladrar():
+    print("no tengo perro")
